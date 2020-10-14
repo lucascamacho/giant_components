ac03a1a68185085d757abd819b50ab1a09fe6d88
katemaia <kate.spirogyra@gmail.com>
2020-10-14 15:10:34 -0300

    kate commitando depois de um teste

---
+++ 1 - primeiro script.R
@@ -0,0 +1,12 @@
+# primeiro script para teste
+
+# # salvar dados quaisquer
+vec = c(1,2,3,4)
+
+save(vec, file = "dados.RData")
+
+pdf("1 - primeiro_plot.pdf")
+plot(vec)
+dev.off()
+#teste commit leandro - teste 2 sem pull req
+# ae ae ae ê ê ê ôooôô
