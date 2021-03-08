fda5262afe52be22d5c5fd83cd32d23828e6df87
Leandro Giacobelli Cosmo <71904120+lgcosmo@users.noreply.github.com>
2021-03-08 16:01:37 -0300

    Update script create uniform random bipartite networks

--- code/5-criar_redes_bipartidas.R
+++ code/5-criar_redes_bipartidas.R
@@ -1,9 +1,10 @@
 library(igraph)
+library(here)
 
 #definir numero de especies e numero de redes aleatorias que queremos
-n_net <- 200
-n_plant <- 100
-n_pol <- 100
+n_net <- 100
+n_plant <- 50
+n_pol <- 50
 
 
 #criar vetores e listas vazias
@@ -15,7 +16,7 @@ num <- vector()
 for(i in 1:n_net){
  
   #prob <- runif(1, 0, (log(n_plant + n_pol)/(n_plant + n_pol))) #probabilidade de conexão
-  prob <- runif(1, 0, 0.05) #probabilidade de conexão
+  prob <- runif(1, 0, 0.06) #probabilidade de conexão
   
   M <- matrix(prob, nrow = n_plant, ncol = n_pol) # crindo matriz M
   
@@ -34,6 +35,22 @@ for(i in 1:n_net){
 
 #plotar alguns resultados
 plot(probs, tam)
+plot(probs, num)
 abline(v = 1/(n_plant + n_pol), col = "red")
 abline(v = log(n_plant + n_pol)/(n_plant + n_pol), col = "green")
+
+#salvando atributos das redes
+bip_names<-paste("BIP", paste(100, 1:100, sep="_"), sep="-")
+net_str<-data.frame(id=bip_names, prob=probs, ncomp=num, sizelargcomp=tam)
+
+write.table(net_str, here("data", "Bip-100_NetStructure.txt"), sep="\t")
+
+#salvando redes
+
+files_names=paste0(bip_names, ".txt")
+
+for (i in 1:length(nets)){
   
+  write.table(nets[[i]], here("data","bip_matrices", files_names[i]), sep="\t", col.names=FALSE, row.names=FALSE)
+  
+}
